90d219af9939682383dc88133e32787ad2627c40
jk_austin <jessekorff@gmail.com>
2025-11-03 09:58:25 -0500

    revert urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,21 +1,18 @@
 # Uncomment the imports before you add the code
-from django.urls import path
+# from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
-from . import views
+# from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
-    
-    # API endpoints with different paths
-    path('api/login/', views.login_user, name='login_user'),
-    path('api/logout/', views.logout_request, name='logout'),
-    
+    # # path for registration
+
+    # path for login
+    # path(route='login', view=views.login_user, name='login'),
+
     # path for dealer reviews view
 
     # path for add a review view
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    
